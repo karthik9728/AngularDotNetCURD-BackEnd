4027208f84ed394548c275819cb30d86f7220721
Karthik <56579449+karthik9728@users.noreply.github.com>
2022-04-19 09:49:36 +0530

    Base Project File

--- InspectionAPIApp/InspectionAPI/Program.cs
+++ InspectionAPIApp/InspectionAPI/Program.cs
@@ -1,9 +1,4 @@
-using Inspections.Infrastructure.Common;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
-var myAllowSpecificOrigins = "_myAllowSpecificOrigins";
-
 
 // Add services to the container.
 
@@ -11,26 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-//Data Base Connection
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-//Enable Cross Platform CORS Crossplatfrom Resource Sharing
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: myAllowSpecificOrigins,
-        builder =>
-        {
-            builder.WithOrigins("http//localhost:4200")
-            .AllowAnyMethod()
-            .AllowAnyHeader();
-        });
-});
-
-
 
 var app = builder.Build();
 
@@ -42,8 +17,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.UseCors(myAllowSpecificOrigins);
 
 app.UseAuthorization();
 
